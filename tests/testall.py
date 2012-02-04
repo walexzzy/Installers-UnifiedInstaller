66c448838ea31386b5baeaf774853ca5fd10a501
Michael Mulich <michael.mulich@gmail.com>
2012-02-04 10:22:24 -0500

    Configure set up information in the test declaration rather than in the test itself. This allows the testing workspace to be dynamic rather than a static location.

--- tests/testall.py
+++ tests/testall.py
@@ -1,11 +1,50 @@
 #!/usr/bin/env python
+"""\
+Test the build installs as expected.
 
-# $LastChangedDate: 2009-02-12 09:26:46 -0800 (Thu, 12 Feb 2009) $ $LastChangedRevision: 25050 $
+Run with environment the TMPDIR environment variable set to keep the
+temporary build with your user space. For example:
 
+    $ TMPDIR=~/tmp python testall.py
 
+"""
+import os
+import sys
+import subprocess
+import tempfile
+import shutil
 import doctest
 
 
-doctest.testfile("tests.txt", optionflags=doctest.ELLIPSIS or NORMALIZE_WHITESPACE)
+HERE = os.path.dirname(__file__)
+INSTALLER_ROOT = os.path.abspath(os.path.join(HERE, '..'))
 
-print 'Done.'
+def doCommand(command):
+    """A simple function that will run the given command string in the
+    installer's working directory."""
+    proc = subprocess.Popen(command,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            cwd=INSTALLER_ROOT,
+                            shell=True)
+    out, err = proc.communicate()
+    return (out, err, proc.returncode)
+
+# Note: Check your tempfile.gettempdir() path length if you get
+#       'AF_UNIX path to long' errors.
+_testTarget = tempfile.mkdtemp()
+DOCTEST_GLOBALS = {
+    'doCommand': doCommand,
+    'testTarget': _testTarget,
+    'withPython': sys.executable,
+    }
+
+optionflags = doctest.ELLIPSIS or NORMALIZE_WHITESPACE
+
+doctest.testfile("tests.txt",
+                 optionflags=optionflags,
+                 globs=DOCTEST_GLOBALS,
+                 )
+
+# Clean up
+shutil.rmtree(_testTarget)
+sys.exit(0)

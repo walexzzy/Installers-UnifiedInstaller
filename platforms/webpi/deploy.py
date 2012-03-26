68cf4eaa48d91e28ac29c2171dbac2fe3a2cb374
Ross Patterson <me@rpatterson.net>
2012-03-26 03:21:39 -0700

    popen args as list

--- platforms/webpi/deploy.py
+++ platforms/webpi/deploy.py
@@ -6,11 +6,11 @@ import subprocess
 
 def main():
     # Run bootstrap
-    subprocess.check_call(sys.executable, os.path.join(
-        os.path.dirname(__file__), 'bootstrap.py', '-d'))
+    subprocess.check_call([sys.executable, os.path.join(
+        os.path.dirname(__file__), 'bootstrap.py', '-d')])
     # Run buildout
-    subprocess.check_call(os.path.join(
-        os.path.dirname(__file__), 'bin', 'buildout.exe', '-N'))
+    subprocess.check_call([os.path.join(
+        os.path.dirname(__file__), 'bin', 'buildout.exe', '-N')])
     
 
 if __name__ == '__main__':

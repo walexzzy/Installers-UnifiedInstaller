606eb609c7d6ce9090f0381399c9cbd1f4362288
Ross Patterson <me@rpatterson.net>
2012-03-26 03:49:37 -0700

    Windows is still not waiting for bootstrap to finish, trying to be more explicit

--- platforms/webpi/deploy.py
+++ platforms/webpi/deploy.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import logging
+import subprocess
 
 logger = logging.getLogger('plone.webdeploy')
 
@@ -13,12 +14,18 @@ def main():
         os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
         'zeocluster', 'bootstrap.py'), '-d']
     logger.info('Running bootstrap: %r' % ' '.join(bootstrap))
-    assert os.system(' '.join(bootstrap)) == 0, 'Bootstrap failed'
+    bootstrap_p = subprocess.Popen(bootstrap)
+    bootstrap_p.communicate()
+    assert bootstrap_p.returncode == 0, (
+        'Bootstrap failed with returncode %s' % bootstrap_p.returncode)
     # Run buildout
     buildout = [os.path.join(
         os.path.dirname(__file__), 'bin', 'buildout.exe'), '-N']
     logger.info('Running buildout: %r' % ' '.join(buildout))
-    assert os.system(' '.join(buildout)) == 0, 'Buildout failed'
+    buildout_p = subprocess.Popen(buildout)
+    buildout_p.communicate()
+    assert buildout_p.returncode == 0, (
+        'Buildout failed with returncode %s' % buildout_p.returncode)
     
 
 if __name__ == '__main__':

d171332cf4247b66705bb75b35aeebf50710b8bc
Ross Patterson <me@rpatterson.net>
2012-03-25 23:54:48 -0700

    Add a very simple Web PI deployment script

---
+++ platforms/webpi/deploy.py
@@ -0,0 +1,17 @@
+"""Deplpy as a Web Deploy package for Microsoft Web Platform Installer."""
+
+import sys
+import os
+import subprocess
+
+def main():
+    # Run bootstrap
+    subprocess.check_call(sys.executable, os.path.join(
+        os.path.dirname(__file__), 'bootstrap.py', '-d'))
+    # Run buildout
+    subprocess.check_call(os.path.join(
+        os.path.dirname(__file__), 'bin', 'buildout.exe', '-N'))
+    
+
+if __name__ = '__main__':
+    main()

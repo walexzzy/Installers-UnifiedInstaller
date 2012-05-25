bdef4efcf40faca93f208f39a8ab252602285601
Ross Patterson <me@rpatterson.net>
2012-05-25 10:38:20 -0400

    Try abusing setup.py to make the zip package

---
+++ platforms/webpi/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+version = '4.2'
+
+setup(name='PloneInstaller',
+      version=version,
+      description="Installer for the Plone Content Management System",
+      classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        ],
+      keywords='Plone CMF python Zope',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://plone.org/',
+      license='GPL version 2',
+      zip_safe=True,
+      )

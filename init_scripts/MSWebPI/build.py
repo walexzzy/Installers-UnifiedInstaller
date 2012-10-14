7e0c00fd198b6d931b0d925aab370f036a8b2517
Ross Patterson <me@rpatterson.net>
2012-10-14 03:06:26 -0700

    Begin MS WebPI build script

---
+++ init_scripts/MSWebPI/build.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import subprocess
+
+
+def main():
+    PLONE_HOME = os.path.abspath(os.path.dirname(__file__))
+    UIDIR = os.path.dirname(os.path.dirname(PLONE_HOME))
+    INSTANCE_HOME = os.path.join(
+        UIDIR, 'init_scripts', 'MSWebPI', 'Plone.msdeploy', 'zinstance')
+    CLIENT_USER = os.environ['USERNAME']
+    ZEO_USER = ROOT_INSTALL = OFFLINE = CLIENTS = 0
+    RUN_BUILDOUT = 1
+    INSTALL_LXML = "no"
+    ITYPE = "standalone"
+    LOG_FILE = os.path.join(PLONE_HOME, 'install.log')
+
+    PASSWORD = subprocess.check_output([
+        sys.executable,
+        os.path.join(UIDIR, 'helper_scripts', 'generateRandomPassword.py')])
+
+    subprocess.check_call([
+        sys.executable,
+        os.path.join(UIDIR, 'helper_scripts', 'create_instance.py'),
+        UIDIR, PLONE_HOME, INSTANCE_HOME, CLIENT_USER, ZEO_USER,
+        PASSWORD, ROOT_INSTALL, RUN_BUILDOUT, INSTALL_LXML, OFFLINE,
+        ITYPE, LOG_FILE, CLIENTS])
+
+if __name__ == '__main__':
+    main()

d452fddbc1ca1ddd122b0574c8aed6e657260945
Ross Patterson <me@rpatterson.net>
2012-10-14 03:57:59 -0700

    Bootstrap and run buildout

--- init_scripts/MSWebPI/build.py
+++ init_scripts/MSWebPI/build.py
@@ -15,6 +15,8 @@ def main():
     INSTALL_LXML = "no"
     ITYPE = "standalone"
     LOG_FILE = os.path.join(PLONE_HOME, 'install.log')
+    BUILDOUT_DIST = os.path.join(
+        PLONE_HOME, 'buildout-cache', 'downloads', 'dist')
 
     PASSWORD = subprocess.check_output([
         sys.executable,
@@ -28,6 +30,20 @@ def main():
         UIDIR, PLONE_HOME, INSTANCE_HOME, CLIENT_USER, ZEO_USER,
         PASSWORD, ROOT_INSTALL, RUN_BUILDOUT, INSTALL_LXML, OFFLINE,
         ITYPE, LOG_FILE, CLIENTS])
+
+    if not os.path.exists(BUILDOUT_DIST):
+        os.makedirs(BUILDOUT_DIST)
+
+    try:
+        os.chdir(INSTANCE_HOME)
+        run_buildout()
+    finally:
+        os.chdir(CWD)
+
+
+def run_buildout():
+    subprocess.check_call([sys.executable, 'bootstrap.py', '-d'])
+    subprocess.check_call([os.path.join('bin', 'buildout.exe'), '-ND'])
 
 if __name__ == '__main__':
     main()
